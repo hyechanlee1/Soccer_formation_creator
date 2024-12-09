d6b086cacf7fc7839e31338f9dfc8edd52db6391
Max Wu <mfw8@calvin.edu>
2024-12-09 12:00:22 -0500

    created formation component, needs images for each formation in assets/images

--- soccer-formation/src/app/app.routes.ts
+++ soccer-formation/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CreateFormationComponent } from './createformation/createformation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/create-formation', pathMatch: 'full' },
+    { path: 'create-formation', component: CreateFormationComponent },
+  ];
---
+++ soccer-formation/src/app/createformation/createformation.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-formation',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './createformation.component.html',
+  styleUrl: './createformation.component.css'
+})
+export class CreateFormationComponent {
+  formations: { [key: string]: string } = {
+    "4-4-2": "assets/images/placeholder1.jpg",
+    "4-3-3": "assets/images/placeholder1.jpg"
+    // add more formations and upload correct image for each formation
+  };
+
+  availableFormations = Object.keys(this.formations);
+  currentFormation = this.availableFormations[0]; // Default to the first formation
+
+  backgroundImage(): string {
+    return `url('${this.formations[this.currentFormation]}')`;
+  }
+
+  onFormationChange(event: Event) {
+    this.currentFormation = (event.target as HTMLSelectElement).value;
+  }
+}
