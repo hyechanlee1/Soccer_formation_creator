c5fdf2c8fe2ce3917338c21d5bc052a7389efebb
Hye Chan Lee <hl63@calvin.edu>
2024-12-11 16:30:39 -0500

    updated

--- soccer-formation/src/app/app.routes.ts
+++ soccer-formation/src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
-import { CreateFormationComponent } from './createformation/createformation.component';
 import { HomeComponent } from './home/home/home.component';
 import { SavedformationsComponent } from './savedformations/savedformations/savedformations.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'create-formation', component: CreateFormationComponent, pathMatch: 'full'},
-    { path: 'saved-formations', component: SavedformationsComponent, pathMatch: 'full'}
-  ];
+  { path: '', component: HomeComponent },
+  { path: 'create-formation', component: HomeComponent, pathMatch: 'full' },
+  { path: 'saved-formations', component: SavedformationsComponent, pathMatch: 'full' }
+];
